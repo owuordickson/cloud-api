6e456e0de2a79f8cf74be9e4d277e7975545eaaa
owuordickson <owuordickson@gmail.com>
2019-10-04 12:43:22 +0200

    fine-tuned modal view

--- custom_dash/content/js/controllers/modal-instance-controller.js
+++ custom_dash/content/js/controllers/modal-instance-controller.js
@@ -1,19 +1,12 @@
 gostApp.controller('ModalInstanceCtrl', function($uibModalInstance, $scope, $http, params) {
   
     $scope.reps = [0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
-    $scope.refs = [];
 
     $scope.data = params;
     $scope.data.steps = 20;
     $scope.data.combs = 100;
     $scope.data.m_rep = 0.5;
-    $scope.data.c_ref = 1;
-
-    var count = 0;
-    angular.forEach($scope.data.crossingList, function(value, key){
-        count = count + 1;
-        $scope.refs.push(count);
-    });
+    $scope.data.c_ref = 0;
 
     $scope.repClicked = function(sel_rep){
         $scope.data.m_rep = sel_rep;
